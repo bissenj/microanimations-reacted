cf60638fa6ea640dd00920d4684e96c77af4229b
bissenj <bissenj@gmail.com>
2022-11-11 09:54:26 -0700

    Updated notes and projects

--- src/modules/common/components/horizontalSlider/HorizontalSlider.jsx
+++ src/modules/common/components/horizontalSlider/HorizontalSlider.jsx
@@ -38,10 +38,38 @@ function HorizontalSlider({index}) {
             </div>
         </div>
     );
+
 }
 
 export default HorizontalSlider;
 
 
-// References:
-// 1.  https://medium.com/tinyso/how-to-create-the-responsive-and-swipeable-carousel-slider-component-in-react-99f433364aa0
+// LET THE LEARNING BEGIN (...continue) -> References
+
+// 1.  React Touch Drag Slider.  Pretty good example of what I want to build.
+//     https://www.npmjs.com/package/react-touch-drag-slider
+//     https://github.com/bushblade/react-touch-drag-slider/tree/main/src
+
+
+
+// 2.  Clever way to move slides on index changed.  No drag support though.
+//     https://medium.com/tinyso/how-to-create-the-responsive-and-swipeable-carousel-slider-component-in-react-99f433364aa0
+
+// 3.  Example of drag slider.  No snap to slide though.
+//     https://codesandbox.io/s/j356wz6wy5?file=/src/Carousel.js
+
+// 4.  React Draggable Slider - Open source project to check out.  Uses React Spring and GSAP
+//     https://www.npmjs.com/package/react-draggable-slider
+
+// 5.  Bear Carousel - Text content fade ins when animations finish
+//     https://carousel.bearests.com/example/text-animations
+//     https://github.com/imagine10255/bear-react-carousel
+
+// 6.  useLayoutEffect
+//     https://upmostly.com/tutorials/react-uselayouteffect-hook?gclid=Cj0KCQiAgribBhDkARIsAASA5bu64wv1TgvjfgbuT2-J4clO126hP7aH-gLlTcCFN4OQbZqMQQwi-4IaAnSDEALw_wcB
+
+// 7.  will-change -> Might help with about page animations
+//     https://css-tricks.com/almanac/properties/w/will-change/
+
+
+
